da3e73368e939ea77d0fc0c84280e66255de420b
SUF <suf@freemail.hu>
2017-12-02 06:48:06 +0100

    Almost finished software for V1 missing error handling for both sense high

--- SW/LineTest/LineTest.ino
+++ SW/LineTest/LineTest.ino
@@ -3,14 +3,29 @@
 #define MainsSwitchCross 2
 #define SenseNeutral 0
 #define SenseLine 1
+#define OnOffSwitch 7
 #define LED_Yellow 10
 #define LED_Blue 8
 #define LED_Green 9
 
 byte NeutralCount;
 byte LineCount;
+byte NoEarthCount;
+byte OnOffCount;
+byte DeadTime;
+bool OnOffTriggered;
 bool StraightSwitchState;
 bool CrossSwitchState;
+
+byte LedState;
+/*
+ * Bit
+ * 0-1  Yellow (0 - Off, 1 - On, 2 - Blink)
+ * 2-3  Green  (0 - Off, 1 - On, 2 - Blink)
+ * 4-5  Blue   (0 - Off, 1 - On, 2 - Blink)
+ * 6    Green/Blue Blink
+ */
+ byte LedCounter;
 
 void setup()
 {
@@ -23,15 +38,93 @@ void setup()
   pinMode(LED_Green, OUTPUT);
   NeutralCount = 0;
   LineCount = 0;
+  OnOffCount = 0;
+  OnOffTriggered = false;
+  LedState = 0;
+  LedCounter = 0;
+  NoEarthCount = 0;
   digitalWrite(MainsSwitchStraight, LOW);
   digitalWrite(MainsSwitchCross, LOW);
   StraightSwitchState = false;
   CrossSwitchState = false;
+  DeadTime = 0;
+}
+
+void LedWorker()
+{
+  LedCounter++;
+  switch(LedState & 0x03)
+  {
+      case 0:
+        digitalWrite(LED_Yellow, LOW);
+        break;
+      case 1:
+        digitalWrite(LED_Yellow, HIGH);
+        break;
+      case 2:
+        digitalWrite(LED_Yellow, (LedCounter > 5) ? HIGH : LOW);
+        break;
+  }
+  if(LedState & 0x40)
+  {
+    if(LedCounter > 5)
+    {
+      digitalWrite(LED_Green, LOW);
+      digitalWrite(LED_Blue, HIGH);
+    }
+    else
+    {
+      digitalWrite(LED_Green, HIGH);
+      digitalWrite(LED_Blue, LOW);
+    }
+  }
+  else
+  {
+    switch(LedState & 0x0C)
+    {
+        case 0:
+          digitalWrite(LED_Green, LOW);
+          break;
+        case 4:
+          digitalWrite(LED_Green, HIGH);
+          break;
+        case 8:
+          digitalWrite(LED_Green, (LedCounter > 5) ? HIGH : LOW);
+          break;
+    }
+    switch(LedState & 0x30)
+    {
+        case 0:
+          digitalWrite(LED_Blue, LOW);
+          break;
+        case 0x10:
+          digitalWrite(LED_Blue, HIGH);
+          break;
+        case 0x20:
+          digitalWrite(LED_Blue, (LedCounter > 5) ? HIGH : LOW);
+          break;
+    }
+    
+  }
+  if(LedCounter > 9)
+    LedCounter = 0;
+}
+
+void SwitchOff()
+{
+    StraightSwitchState = false;
+    CrossSwitchState = false;
+    digitalWrite(MainsSwitchStraight, LOW);
+    digitalWrite(MainsSwitchCross, LOW);
+    LedState = 0;
+    DeadTime=10;
 }
 
 void loop()
 {
-  if(digitalRead(SenseNeutral == HIGH))
+  if(DeadTime != 0)
+    DeadTime--;
+  if(digitalRead(SenseNeutral) == HIGH)
   {
     if(NeutralCount < 10)
     {
@@ -42,7 +135,7 @@ void loop()
   {
     NeutralCount = 0;
   }
-  if(digitalRead(SenseLine == HIGH))
+  if(digitalRead(SenseLine) == HIGH)
   {
     if(LineCount < 10)
     {
@@ -53,61 +146,115 @@ void loop()
   {
     LineCount = 0;
   }
+
+  if(digitalRead(OnOffSwitch) == LOW)
+  {
+    if(OnOffCount < 10)
+    {
+      OnOffCount++;
+      OnOffTriggered = (OnOffCount == 10);
+    }
+  }
+  else
+  {
+    OnOffCount = 0;
+  }
+  // Count the non-Earthed state
+  if(LineCount == 0 && NeutralCount == 0)
+  {
+    if(NoEarthCount < 10)
+    {
+      NoEarthCount++;
+    }
+  }
+  else
+  {
+    NoEarthCount = 0;
+  }
+  
   if(StraightSwitchState || CrossSwitchState)
   {
     // system was online
-    if(StraightSwitchState)
+    if(NoEarthCount < 10)
     {
-      if(LineCount < 10)
+      if(StraightSwitchState)
       {
-        StraightSwitchState = false;
-        CrossSwitchState = false;
-        digitalWrite(MainsSwitchStraight, LOW);
-        digitalWrite(MainsSwitchCross, LOW);
-        digitalWrite(LED_Green, LOW);
-        digitalWrite(LED_Blue, LOW);
-        delay(1000);
+        if(LineCount < 10)
+        {
+          SwitchOff();
+        }
       }
-    }
-    if(CrossSwitchState)
-    {
-      if(NeutralCount < 10)
+      if(CrossSwitchState)
       {
-        StraightSwitchState = false;
-        CrossSwitchState = false;
-        digitalWrite(MainsSwitchStraight, LOW);
-        digitalWrite(MainsSwitchCross, LOW);
-        digitalWrite(LED_Green, LOW);
-        digitalWrite(LED_Blue, LOW);
-        delay(1000);
+        if(NeutralCount < 10)
+        {
+          SwitchOff();
+        }
       }
     }
     if(LineCount >= 10 && NeutralCount >= 10)
     {
-      StraightSwitchState = false;
-      CrossSwitchState = false;
-      digitalWrite(MainsSwitchStraight, LOW);
-      digitalWrite(MainsSwitchCross, LOW);
-      digitalWrite(LED_Green, LOW);
-      digitalWrite(LED_Blue, LOW);
-      delay(1000);
+      SwitchOff();
+    }
+    if(OnOffTriggered)
+    {
+      SwitchOff();
+      OnOffTriggered = false;
     }
   }
   else
   {
     // The system was offline
-    if(LineCount >= 10 && NeutralCount == 0)
+    if(DeadTime == 0)
     {
-      digitalWrite(MainsSwitchStraight, HIGH);
-      digitalWrite(LED_Green, HIGH);
-      StraightSwitchState = true;
-    }
-    if(NeutralCount >= 10 && LineCount == 0)
-    {
-      digitalWrite(MainsSwitchCross, HIGH);
-      digitalWrite(LED_Blue, HIGH);
-      CrossSwitchState = true;
+      if(LineCount >= 10 && NeutralCount == 0)
+      {
+        if(OnOffTriggered)
+        {
+          digitalWrite(MainsSwitchStraight, HIGH);
+          // Switch on Green;
+          LedState = 0x04;
+          StraightSwitchState = true;
+          OnOffTriggered = false;
+        }
+        else
+        {
+          // Blink Green;
+          LedState = 0x08;
+        }
+      }
+      if(NeutralCount >= 10 && LineCount == 0)
+      {
+        if(OnOffTriggered)
+        {
+          digitalWrite(MainsSwitchCross, HIGH);
+          // Switch on Blue
+          LedState = 0x10;
+          CrossSwitchState = true;
+          OnOffTriggered = false;
+        }
+        else
+        {
+          // Blink Blue
+          LedState = 0x20;
+        }
+      }
+      if(NoEarthCount >= 10)
+      {
+        if(OnOffTriggered)
+        {
+          digitalWrite(MainsSwitchStraight, HIGH);
+          LedState = 0x16;
+          StraightSwitchState = true;
+          OnOffTriggered = false;
+        }
+        else
+        {
+          LedState = 0x42;
+        }
+      }
     }
   }
+  LedWorker();
   delay(100);
 }
